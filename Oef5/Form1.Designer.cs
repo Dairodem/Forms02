2b72cfb24b3acb716a669d3c023a320d02130b8c
demeulemeester dairo <dairo_demeulemeester@hotmail.com>
2020-08-12 14:53:49 +0200

    update na oef5

---
+++ Oef5/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Oef5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbxItemList = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.txtNewItem = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbxItemList
+            // 
+            this.lbxItemList.FormattingEnabled = true;
+            this.lbxItemList.ItemHeight = 16;
+            this.lbxItemList.Location = new System.Drawing.Point(12, 12);
+            this.lbxItemList.Name = "lbxItemList";
+            this.lbxItemList.Size = new System.Drawing.Size(143, 180);
+            this.lbxItemList.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(161, 41);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(146, 26);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Toevoegen";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(160, 70);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(147, 26);
+            this.btnRemove.TabIndex = 2;
+            this.btnRemove.Text = "Verwijderen";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // txtNewItem
+            // 
+            this.txtNewItem.Location = new System.Drawing.Point(161, 13);
+            this.txtNewItem.Name = "txtNewItem";
+            this.txtNewItem.Size = new System.Drawing.Size(146, 22);
+            this.txtNewItem.TabIndex = 3;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(12, 198);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(143, 26);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Opslaan";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(12, 227);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(143, 26);
+            this.btnLoad.TabIndex = 5;
+            this.btnLoad.Text = "Laden";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(319, 263);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtNewItem);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lbxItemList);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Oefening 5";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbxItemList;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.TextBox txtNewItem;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnLoad;
+    }
+}
+
---
+++ Oef5/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Oef5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (txtNewItem.Text != null)
+                lbxItemList.Items.Add(txtNewItem.Text);
+
+            txtNewItem.Text = "";
+            txtNewItem.Focus();
+
+
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (lbxItemList.SelectedIndex >= 0)
+                lbxItemList.Items.RemoveAt(lbxItemList.SelectedIndex);
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            using (StreamWriter writer = new StreamWriter("ItemsList.txt"))
+            {
+                for (int i = 0; i < lbxItemList.Items.Count; i++)
+                {
+                    writer.WriteLine(lbxItemList.Items[i]);
+
+                }
+            }
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            lbxItemList.Items.Clear();
+
+            using (StreamReader reader = new StreamReader("ItemsList.txt"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    lbxItemList.Items.Add(reader.ReadLine());
+                }
+
+            }
+
+        }
+    }
+}
